33b548ed2bd30ef2340d54c5be8b7f3690ae055c
jaresj <justice.jares@gmail.com>
2022-08-21 19:55:39 -0700

    added headed and sign in Auth

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const { engine } = require('express-handlebars');
+const passport = require('passport');
+const session = require('express-session');
 const connectDB = require('./config/db');
 
 // Load config
 dotenv.config({ path: './config/config.env' });
+
+// Passport config
+require('./config/passport')(passport);
 
 connectDB();
 
@@ -20,8 +26,25 @@ if (process.env.NODE_ENV === 'development') {
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }));
 app.set('view engine', '.hbs');
 
+// Sessions
+app.use(
+  session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Static foler
+app.use(express.static(path.join(__dirname, 'public')));
+
 // ROUTES
 app.use('/', require('./routes/index'));
+app.use('/auth', require('./routes/auth'));
 
 const PORT = process.env.PORT || 3000;
 
