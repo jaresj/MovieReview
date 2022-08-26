973471ba8a03ce9a5f6fe0245c3352f1c5fb2aae
jaresj <justice.jares@gmail.com>
2022-08-26 14:23:11 -0700

    first draft of MVC

--- routes/auth.js
+++ routes/auth.js
@@ -18,9 +18,13 @@ router.get(
 
 // @desc Logout user
 // @route /auth/logout
-router.get('/logout', (res, req) => {
-  req.logout();
-  res.redirect('/');
+router.get('/logout', (req, res, next) => {
+  req.logout((error) => {
+    if (error) {
+      return next(error);
+    }
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
